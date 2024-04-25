3cc71844ed0b45a24ffe125a6e7e86578d6f78bc
R1verb <128699019+R1verb@users.noreply.github.com>
2024-04-26 01:15:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-
+document.getElementById('copyLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Отменяем стандартное действие перехода по ссылке
+    var textToCopy = this.textContent; // Получаем текст ссылки
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        var copyNotification = document.getElementById('copyNotification');
+        copyNotification.style.display = 'block';
+        setTimeout(function() {
+            copyNotification.style.display = 'none';
+        }, 2000); // Скрыть сообщение через 2 секунды
+    }, function(err) {
+        console.error('Не удалось скопировать текст: ', err);
+    });
+});
